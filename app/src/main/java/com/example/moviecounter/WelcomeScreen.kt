a959587abb39941d578b945bd8a9147509c00a3a
treicypuma <treicy.puma@tecsup.edu.pe>
2026-09-08 15:33:21 -0500

    feat: add WelcomeScreen component

---
+++ app/src/main/java/com/example/moviecounter/WelcomeScreen.kt
@@ -0,0 +1,29 @@
+package com.example.moviecounter
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WelcomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "¡Bienvenido a MovieCounter!")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewWelcomeScreen() {
+    WelcomeScreen()
+}
